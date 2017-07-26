85ae09eab8a712d4ed5dc055acd4bcc4f9cb9a76
andy <andy@jonezie.com>
2017-07-26 00:06:24 -0400

    prelim working

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ if (typeof web3 !== 'undefined') {
   web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
 }
 
+var punkOwners={}
+var punkOwnersCollected = false;
+
  function init()
  {
 
@@ -45,14 +48,9 @@ if (typeof web3 !== 'undefined') {
       //  console.log('contract_abi')
         //console.log(contract_abi)
 
-        try
-        {
-          loadContract(contract_abi);
-        }
-        catch(e)
-        {
-          console.error(e);
-        }
+
+          readPunkOwnersFromContract(contract_abi);
+
 
 
 
@@ -67,14 +65,8 @@ if (typeof web3 !== 'undefined') {
          document.getElementById('result').textContent = JSON.stringify(x, null, 2);
      };
 
-     var loadContract = function (contract_abi) {
+     var readPunkOwnersFromContract = function (contract_abi) {
          // let's assume that we have a private key to coinbase ;)
-        /* web3.eth.defaultAccount = web3.eth.coinbase;
-
-         document.getElementById('create').style.visibility = 'hidden';
-         document.getElementById('status').innerText = "transaction sent, waiting for confirmation";*/
-    //     web3.eth.contract(abi).new({data: code}, function (err, c) {
-
 
 
 
@@ -91,64 +83,36 @@ if (typeof web3 !== 'undefined') {
             //    console.log(contractInstance)
 
 
+
+        var punk_id = 0;
         try
         {
-          var x = contractInstance.nextPunkIndexToAssign.call('');
-          console.log(x)
+          for(punk_id=0;punk_id<10000;punk_id++)
+          {
+            var owner_address = contractInstance.punkIndexToAddress.call(punk_id);
+            punkOwners[punk_id] = owner_address;
+            console.log('owner of punk' + punk_id.toString() + ' is ' +  owner_address.toString() )
+
+          }
+
+
         }
         catch(e)
         {
           console.error(e);
         }
 
+        punkOwnersCollected = true;
+
+        console.log('Completed collection of punk owners.')
 
 
 
 
 
-        /*var assignEvent = contractInstance.Assign({some: 'args'}, {fromBlock: 0, toBlock: 'latest'});
-        var transferEvent = contractInstance.Transfer({some: 'args'}, {fromBlock: 0, toBlock: 'latest'});
-        var transferPunkEvent = contractInstance.PunkTransfer({some: 'args'}, {fromBlock: 0, toBlock: 'latest'});
-
-
-        assignEvent.watch(function(error, result){
-                 console.log(result)
-           });
-
-           // would get all past logs again.
-           var myResults = assignEvent.get(function(error, logs){
-             console.log(logs)
-           });*/
-
-
-      /* -- this work s
-           web3.eth.getBlock(48, function(error, result){
-               if(!error)
-                   console.log(result)
-               else
-                   console.error(error);
-           })
-           */
-
-
-/*
-           var theDAOABIFragment = [{"anonymous":false,"inputs":[{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"amount","type":"uint256"}],"name":"CreatedToken","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_amount","type":"uint256"}],"name":"Transfer","type":"event"}];
-           var theDAOAddress = "0xBB9bc244D798123fDe783fCc1C72d3Bb8C189413";
-           var theDAOStartingBlock = 1428757;
-           var theDAO = web3.eth.contract(theDAOABIFragment).at(theDAOAddress);
-           var theDAOCreatedTokenEvent = theDAO.CreatedToken({}, {fromBlock: theDAOStartingBlock, toBlock: theDAOStartingBlock + 2000});
-           console.log("address\tamount\tto\tblockHash\tblockNumber\tevent\tlogIndex\ttransactionHash\ttransactionIndex");
-           theDAOCreatedTokenEvent.watch(function(error, result){
-             console.log(result.address + "\t" + result.args.amount / 1e16 + "\t" + result.args.to + "\t" +
-               result.blockHash + "\t" + result.blockNumber + "\t" + result.event + "\t" + result.logIndex + "\t" +
-               result.transactionHash + "\t" + result.transactionIndex);
-
-           });
-
-*/
 
             var punkContractBlock = 3914495;
-
+/*
             var punkOwners={}
 
            var contractEventAssign = contractInstance.Assign({}, {fromBlock: punkContractBlock, toBlock: punkContractBlock+100000});
@@ -165,41 +129,11 @@ if (typeof web3 !== 'undefined') {
 
            });
 
+*/
 
 
 
 
-          // would get all past logs again.
-      /*    events.get(function(error, logs){
-            if(error)
-            {
-                 console.error(error)
-            }
-             console.log(JSON.stringify(logs.args))
-           });*/
-
-          /* contractInstance.PunkTransfer({}, { fromBlock: 0, toBlock: 'latest' }).get((error, eventResult) => {
-             if (error)
-               console.log('Error in myEvent event handler: ' + error);
-             else
-               console.log('myEvent: ' + JSON.stringify(eventResult.args));
-           });
-           */
-
-
-        /*  web3.eth.contract(abi).new({}, function (err, c) {
-             if (err) {
-                 console.error(err);
-                 return;
-             // callback fires twice, we only want the second call when the contract is deployed
-             } else if(c.address){
-                 contract = c;
-                 console.log('address: ' + contract.address);
-                 document.getElementById('status').innerText = 'Mined!';
-                 document.getElementById('call').style.visibility = 'visible';
-                 inc = contract.Incremented({odd: true}, update);
-             }
-         });*/
      };
 
 
@@ -223,8 +157,19 @@ if (typeof web3 !== 'undefined') {
        console.log('listening on JSONRPC server localhost:4040')
          // create a server
          var server = jayson.server({
-           add: function(args, callback) {
-             callback(null, args[0] + args[1]);
+           getPunkOwner: function(args, callback) {
+
+             if(punkOwnersCollected == false )
+             {
+               callback(null, 'notSynced');
+             }
+
+
+             var punk_id = args[0];
+             var punk_owner_address = punkOwners[punk_id];
+             callback(null, punk_owner_address);
+
+
            }
          });
 
